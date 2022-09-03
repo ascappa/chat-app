8f4dd50dc7ac0b56894631684c0eb58671cc8d7c
ascappa <108097502+ascappa@users.noreply.github.com>
2022-09-03 17:23:57 +0200

    -changed message number to 25 -improved UI with flexbox and better colors

--- chat.js
+++ chat.js
@@ -4,12 +4,11 @@ const socket = io();
 // First we get the viewport height and we multiple it by 1% to get a value for a vh unit
 let vh = window.innerHeight * 0.01;
 // Then we set the value in the --vh custom property to the root of the document
-document.documentElement.style.setProperty('--vh', `${vh}px`);
+document.documentElement.style.setProperty("--vh", `${vh}px`);
 
 const messages = document.getElementById("messages");
 const form = document.getElementById("form");
 const input = document.getElementById("input");
-
 
 socket.on("load message", ({ msg, nickname, presenceChange = false }) => {
   const message = document.createElement("li");
@@ -23,23 +22,31 @@ socket.on("send nickname", () => {
     localStorage.setItem("nickname", prompt("Hey, what's your nickname?"));
   }
   nickname = localStorage.getItem("nickname");
-  socket.emit("nickname", nickname)
-})
+  socket.emit("nickname", nickname);
+});
 
 form.addEventListener("submit", (e) => {
   e.preventDefault();
   if (input.value) {
     const msg = input.value;
-    socket.emit("chat message", { msg, nickname });
+    socket.emit("own message", { msg, nickname });
     socket.emit("save message", `${nickname}: ${msg}`);
     input.value = "";
   }
 });
 
-socket.on("chat message", async ({ msg, nickname, presenceChange = false }) => {
-  const message = document.createElement("li");
-  console.log(presenceChange);
-  message.textContent = presenceChange ? msg : `${nickname}: ${msg}`;
-  messages.append(message);
-  messages.scrollBy(0, messages.scrollHeight);
-});
+socket.on(
+  "chat message",
+  async ({ msg, nickname, presenceChange = false, own = false }) => {
+    const message = document.createElement("li");
+    console.log(presenceChange);
+    if (own) {
+      message.textContent = `You: ${msg}`;
+      message.className = "own-message"
+    } else {
+      message.textContent = presenceChange ? msg : `${nickname}: ${msg}`;
+    }
+    messages.append(message);
+    messages.scrollBy(0, messages.scrollHeight);
+  }
+);
