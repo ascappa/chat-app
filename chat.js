3d1f31f15d447b866b7953c8d8177920ab9e1093
ascappa <108097502+ascappa@users.noreply.github.com>
2022-09-04 21:48:33 +0200

    Add better comments to code

--- chat.js
+++ chat.js
@@ -6,16 +6,19 @@ let vh = window.innerHeight * 0.01;
 // Then we set the value in the --vh custom property to the root of the document
 document.documentElement.style.setProperty("--vh", `${vh}px`);
 
+/* Getting the elements from the HTML file. */
 const messages = document.getElementById("messages");
 const form = document.getElementById("form");
 const input = document.getElementById("input");
 
+/* A function that is called when the window is resized. It is used to make the chat responsive. */
 window.addEventListener("resize", (e) => {
   vh = window.innerHeight * 0.01;
   document.documentElement.style.setProperty("--vh", `${vh}px`);
   messages.scrollTo(0, messages.scrollHeight);
 });
 
+/* Asking the user for a nickname and then storing it in local storage. */
 let nickname;
 socket.on("send nickname", () => {
   if (!localStorage.getItem("nickname")) {
@@ -25,6 +28,9 @@ socket.on("send nickname", () => {
   socket.emit("nickname", nickname);
 });
 
+/* Listening for a submit event on the form element. When the event is triggered, it prevents the
+default action of the event, which is to reload the page. Then it tells the server to send
+a message to all clients and save it to the database and then it resets the input. */
 form.addEventListener("submit", (e) => {
   e.preventDefault();
   if (input.value) {
@@ -35,6 +41,8 @@ form.addEventListener("submit", (e) => {
   }
 });
 
+/* This function creates the message element for the client and checks what kind of message (generic,
+  presence, own, etc.) it is. */
 socket.on(
   "chat message",
   async ({ content, nickname: messageNickname, presenceChange = false }) => {
@@ -42,12 +50,12 @@ socket.on(
     console.log(presenceChange);
     if (nickname === messageNickname) {
       displayedNickname = "You";
-      message.className = "own-message"
+      message.className = "own-message";
     } else {
       displayedNickname = messageNickname;
     }
     if (presenceChange) {
-      message.className = "presence-message"
+      message.className = "presence-message";
     }
     message.textContent = presenceChange
       ? `${displayedNickname} ${content}`
--- index.js
+++ index.js
@@ -1,9 +1,12 @@
+/* This is the code that is used to setup the server. */
 const express = require("express");
 const http = require("http");
 const { default: mongoose } = require("mongoose");
+/* Creating an HTTP server and passing the express app to it. */
 const app = express();
-const { Server } = require("socket.io");
 const server = http.createServer(app);
+/* Socket.io takes an instance of an HTTP server that it upgrades to a websocket server. */
+const { Server } = require("socket.io");
 const io = new Server(server);
 const PORT = process.env.PORT || 3000;
 const Message = require("./models/Message.js");
@@ -18,15 +21,21 @@ app.get("/", (req, res) => {
 // socket.io connection handler
 io.on("connection", async (socket) => {
   console.log("connected");
+  /* Getting the last 25 messages from the database. */
   let messages = await Message.find({});
   messages = messages.slice(-25);
 
+  /* A function that is called when the client sends a nickname. It is used to send the last 25
+  messages to the client and to send a message to all clients when a client joins or leaves the
+  chat. */
   socket.on("nickname", async (nickname) => {
     console.log("nickname received");
+    /* Sending the last 25 messages to the client. */
     messages.forEach((message) => {
       const { content, nickname, presenceChange } = message;
       socket.emit("chat message", { content, nickname, presenceChange });
     });
+    /* Sending a message to all clients when a client joins the chat and saving it to database. */
     const welcomeMsg = ` joined the chat 😁`;
     const goodbyeMsg = ` left the chat 🥶`;
     io.emit("chat message", {
@@ -40,6 +49,7 @@ io.on("connection", async (socket) => {
       presenceChange: true,
     });
 
+    /* Sending a message to all clients when a client leaves the chat and saving it to database. */
     socket.on("disconnect", async () => {
       io.emit("chat message", {
         content: goodbyeMsg,
@@ -54,19 +64,24 @@ io.on("connection", async (socket) => {
     });
   });
 
+  /* Telling the client to send the nickname. */
   socket.emit("send nickname");
 
+  /* A function that is called when the client wants to save a message to the database. */
   socket.on("save message", async ({ content, nickname }) => {
     await Message.create({ content, nickname });
   });
 
+  /* A function that is called when the client sends a message. It is used to send the message to all
+  clients. */
   socket.on("chat message", ({ content, nickname }) => {
     io.emit("chat message", { content, nickname });
   });
-
-  socket.on("test", (test) => console.log(test));
 });
 
+/**
+ * If the connection to the database is successful, then start the server.
+ */
 async function run() {
   try {
     await mongoose.connect(process.env.ATLAS_URI);
